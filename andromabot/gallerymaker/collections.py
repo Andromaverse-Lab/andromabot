61029c0dea31aea64815d09ba33b1055800b79ac
Starship IBC <shrugs.rest0x@icloud.com>
2022-08-16 22:53:18 -0400

    Pluralize collections

--- andromabot/gallerymaker/collections.py
+++ andromabot/gallerymaker/collections.py
@@ -2,16 +2,16 @@ from enum import Enum
 
 
 class AndromedaCollections(Enum):
-    STARGAZE_PUNK = (0,)
-    ANDROMA_PUNK = (1,)
+    STARGAZE_PUNKS = (0,)
+    ANDROMA_PUNKS = (1,)
     ANDROMAVERSE = 3
 
     @classmethod
     def from_str(cls, str):
         s = str.lower()
         if s in ["stargaze_punk", "stargaze punk", "stargaze-punk", "spunk", "sc"]:
-            return cls.STARGAZE_PUNK
+            return cls.STARGAZE_PUNKS
         if s in ["androma_punk", "androma punk", "androma-punk", "apunk"]:
-            return cls.ANDROMA_PUNK
+            return cls.ANDROMA_PUNKS
         if s in ["andromaverse", "egg"]:
             return cls.ANDROMAVERSE
